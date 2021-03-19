bdf51f8482466e3b9654440fe19855c12bf64ca4
Matthew Ellis <mbellistx@gmail.com>
2021-03-19 14:16:12 -0500

    updated progress comments

--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -33,6 +33,6 @@ function SmurfList(){
 export default SmurfList;
 
 //Task List:
-//1. Connect the smurfs and loading state values to the SmurfList component.
-//2. Replace the single Smurf component instance with a map return a Smurf component for each entry in the smurfs list.
-//3. Replace the static isLoading variable with the state loading variable.
+//1. Connect the smurfs and loading state values to the SmurfList component. DONE
+//2. Replace the single Smurf component instance with a map return a Smurf component for each entry in the smurfs list. DONE
+//3. Replace the static isLoading variable with the state loading variable. DONE
